1ed675c4e90f9e952438bde26f52216190c9d521
BrunaDuarte-3321 <62619506+BrunaDuarte-3321@users.noreply.github.com>
2022-05-02 16:19:52 -0300

    Primeiro dia da NLW.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Widget } from "./components/Widget";
+
+export const App = () =>{
+  return(
+    <div>
+      <Widget/>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brend:{
+          500:'#8257e6',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
